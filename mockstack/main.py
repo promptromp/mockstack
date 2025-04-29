be628b3685af1954bebcc0d1fbbec24bcd0354bf
adamhadani <139888+adamhadani@users.noreply.github.com>
2025-04-28 22:54:58 -0500

    add proper cli via typer pkg

--- mockstack/main.py
+++ mockstack/main.py
@@ -1,14 +1,17 @@
 """Application entrypoints."""
 
 from fastapi import FastAPI
+from typer import Typer
 
 from mockstack.config import settings_provider
 from mockstack.lifespan import lifespan_provider
 from mockstack.middleware import middleware_provider
-from mockstack.opentelemetry import opentelemetry_provider
 from mockstack.routers.catchall import catchall_router_provider
 from mockstack.routers.homepage import homepage_router_provider
 from mockstack.strategies.factory import strategy_provider
+from mockstack.telemetry import opentelemetry_provider
+
+cli = Typer()
 
 
 def create_app() -> FastAPI:
@@ -27,5 +30,23 @@ def create_app() -> FastAPI:
     return app
 
 
-# expose top-level app object for fastapi cli
-app = create_app()
+@cli.command()
+def run(host: str = "0.0.0.0", port: int = 8000):
+    """mockstack run CLI entrypoint."""
+    import uvicorn
+
+    app = create_app()
+    uvicorn.run(app, host=host, port=port)
+
+
+@cli.command()
+def version():
+    """mockstack version CLI entrypoint."""
+    from importlib.metadata import version
+
+    pkg_version = version("mockstack")
+    print(f"mockstack {pkg_version}")
+
+
+if __name__ == "__main__":
+    cli()
